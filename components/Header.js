113961b716664b3155aacfd55b2906f425720086
rutik wankhade <rutikwankhade2@gmail.com>
2022-07-27 10:34:58 +0530

    feat: add logout functionality

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import {
     useSession,
     signIn,
+    signOut
 } from "next-auth/react";
 
 const Header = () => {
@@ -13,19 +14,20 @@ const Header = () => {
             <h1 className="md:text-xl text-md font-semibold">💡 half baked ideas</h1>
 
 
-
-
-
             <div className="ml-auto mr-2">
                 {session && session.user ? (
                     <div className="flex  flex-row items-center">
-                        <span className="mx-2 text-sm text-gray-500">
+                        <span className="mx-2 text-md font-medium text-gray-500">
                             {session.user.name}
                         </span>
 
-
-
-                       
+                        <button
+                            type="button"
+                            className=" mx-2    cursor-pointer "
+                            onClick={() => signOut()}
+                        >
+                           <svg className="w-6 h-6 text-slate-700 hover:text-slate-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
+                        </button>
                     </div>
                 ) : (
                     <button
@@ -34,8 +36,6 @@ const Header = () => {
                         Share your idea
                     </button>
                 )}
-
-
 
             </div>
         </div>
