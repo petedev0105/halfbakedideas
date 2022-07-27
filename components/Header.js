f9c08a0533c03173831fceddaecc647dcec33a0a
rutik wankhade <rutikwankhade2@gmail.com>
2022-07-27 10:28:14 +0530

    feat:yay! setup goole login with next auth

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,45 @@
 import Link from "next/link";
+import {
+    useSession,
+    signIn,
+} from "next-auth/react";
 
-Link
 const Header = () => {
-    return ( 
+    const { data: session, status } = useSession();
+
+    return (
 
         <div className="md:px-52 p-2 flex items-center bg-white top-0 sticky z-10">
-   <h1 className="text-xl font-semibold">💡 half baked ideas</h1>
+            <h1 className="md:text-xl text-md font-semibold">💡 half baked ideas</h1>
+
+
+
+
 
             <div className="ml-auto mr-2">
+                {session && session.user ? (
+                    <div className="flex  flex-row items-center">
+                        <span className="mx-2 text-sm text-gray-500">
+                            {session.user.name}
+                        </span>
 
-                <Link href="/signup">
-                   <button className="text-md text-white bg-pink-300 hover:bg-pink-400 px-4 font-semibold p-2 rounded-lg">Share your idea</button> 
-                </Link>
+
+
+                       
+                    </div>
+                ) : (
+                    <button
+                        onClick={() => signIn('google')}
+                        className="text-md text-white bg-pink-300 hover:bg-pink-400 px-4 font-semibold p-2 rounded-lg">
+                        Share your idea
+                    </button>
+                )}
+
+
 
             </div>
-            </div>
-     );
+        </div>
+    );
 }
- 
+
 export default Header;
--- components/Post.js
+++ components/Post.js
@@ -31,4 +31,6 @@ const Post = () => {
     );
 }
 
+
+
 export default Post;
