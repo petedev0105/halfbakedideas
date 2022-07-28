b85ade007a4980b57bd603849c8ad9fce5e92309
rutik wankhade <rutikwankhade2@gmail.com>
2022-07-28 09:54:18 +0530

    feat: create post idea api

---
+++ pages/api/post.js
@@ -0,0 +1,19 @@
+import prisma from '../../lib/prisma';
+import { getSession } from 'next-auth/react'
+
+// POST /api/post
+// Required fields in body: title, category
+export default async function handle(req, res) {
+
+  const { title, category } = req.body;
+  const session = await getSession({ req });
+  
+    const result = await prisma.post.create({
+    data: {
+      title: title,
+      category: category,
+      author: { connect: { email: session?.user?.email } },
+    },
+  });
+  res.json(result);
+}
--- pages/submit.js
+++ pages/submit.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Image from 'next/image'
 import ideaLogo from '../assets/images/idea-logo.png'
@@ -9,16 +9,38 @@ import { useSession } from 'next-auth/react'
 
 
 const Submit = () => {
-    
-    const { data: session, status } = useSession()
 
+    const { data: session, status } = useSession()
     const router = useRouter()
+
+
+    const [title, setTitle] = useState('')
+    const [category, setCategory] = useState('')
+
+
     useEffect(() => {
         if (!session) {
             router.push('/')
         }
     }, [session])
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const body = { title, category: "productivity" };
+           
+          await fetch('/api/post', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(body),
+         });
+
+            await router.push('/');
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
 
     return (
@@ -30,25 +52,40 @@ const Submit = () => {
                     <Image src={ideaLogo} />
                 </div>
 
-                <div className="mx-auto flex flex-col justify-center p-4 shadow-xl shadow-slate-100 rounded-lg bg-white pt-12">
+                <form onSubmit={handleSubmit}
+                    className="mx-auto flex flex-col justify-center p-4 shadow-xl shadow-slate-100 rounded-lg bg-white pt-12">
 
                     <h1 className="md:text-2xl my-2 text-xl font-semibold text-center">Share your half baked idea</h1>
                     <textarea
+                        onChange={(e) => setTitle(e.target.value)}
                         placeholder="an app that can..."
                         className="h-28 outline-none p-2 rounded-lg mx-auto text-lg border my-2 md:w-10/12 w-full"
                     />
 
+
                     <Select
+                        value={category}
+                        onChange={selectedOption => setCategory(selectedOption)}
+
+                        placeholder="Choose category"
                         options={categoryOptions}
                         styles={style}
                         theme={theme}
                         instanceId='submit'
 
-                        className=" font-semibold border capitalize outline-none rounded-lg text-lg md:w-10/12 mx-auto my-2" />
+                        className=" font-semibold border capitalize outline-none rounded-lg text-lg md:w-10/12 mx-auto my-2"
+                    />
 
-                    <button className="text-lg flex bg-slate-700 hover:scale-105 duration-300 hover:bg-slate-800 mx-auto w-max p-2 px-6 rounded-lg text-white font-semibold">Submit</button>
 
-                </div>
+
+
+
+
+                    <button
+                        type="submit"
+                        className="text-lg flex bg-slate-700 hover:scale-105 duration-300 hover:bg-slate-800 mx-auto w-max p-2 px-6 rounded-lg text-white font-semibold">Submit</button>
+
+                </form>
 
             </div>
         </div>
